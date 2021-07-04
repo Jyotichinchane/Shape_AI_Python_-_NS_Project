913d681b7e5fcc292ef0fc01a52fbf3d26172931
Jyotichinchane <78404024+Jyotichinchane@users.noreply.github.com>
2021-07-04 17:31:47 +0530

    Update md5str2.py

--- md5str2.py
+++ md5str2.py
@@ -1,7 +1,7 @@
 import hashlib
 
 # encode it to bytes using UTF-8 encoding
-message = "Pooja Bhoskar".encode()
+message = "Jyoti Chinchane".encode()
 
 # hash with MD5 (not recommended)
 print("MD5:", hashlib.md5(message).hexdigest())
